46acddafe6ff718ff991cfade1ae4bd8200f23aa
Seb Glazebrook <sebastian.glazebrook@redbubble.com>
2015-11-15 21:02:57 +1100

    Inital thoughts for what a cell will looks like

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,3 +1,23 @@
 class Cell
 
+  def initialize
+    @alive = false
+  end
+
+  def alive?
+    @alive
+  end
+
+  def dead?
+    !alive?
+  end
+
+  def die!
+    @alive = false
+  end
+
+  def live!
+    @alive = true
+  end
+
 end
